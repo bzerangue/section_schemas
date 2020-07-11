e6dfccd2539c33b4e9aaaf112fc400c3cdcd145c
Brian Zerangue <brian.zerangue@gmail.com>
2020-07-10 20:52:48 -0500

    merged in fixes and updated extension.meta file

--- extension.driver.php
+++ extension.driver.php
@@ -4,7 +4,7 @@
 
 	Class Extension_Section_Schemas extends Extension {
 
-		private static $provides = array();
+		protected static $provides = array();
 		
 		public static function registerProviders() {
 			self::$provides = array(
